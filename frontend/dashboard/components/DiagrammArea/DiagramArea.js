21bfcfd35ff71a6587dbd02ef03cad088fa8e152
Allakhverdiev Marat <115146711+Attract1ve@users.noreply.github.com>
2026-03-30 17:51:10 +0300

    added trafficchart

--- frontend/dashboard/components/DiagrammArea/DiagramArea.js
+++ frontend/dashboard/components/DiagrammArea/DiagramArea.js
@@ -1,10 +1,12 @@
 'use client';
 
 import styles from './DiagramArea.module.css';
+import TrafficChart from '../../core/hooks/TrafficChart';
 export default function DiagramArea() {
   return (
     <div className={styles.DiagramContainer}>
       <h2 className={styles.title}>Category Share </h2>
+      <TrafficChart/>
     </div>
   );
 }
