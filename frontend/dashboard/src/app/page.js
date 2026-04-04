0a12ef29015e0c5bdf24a2dfb3c80a39265dffa3
denigo360 <115095299+denigo360@users.noreply.github.com>
2026-04-04 23:38:10 +0300

    Bind frontend and backend(Traffic's Diagramm)

--- frontend/dashboard/src/app/page.js
+++ frontend/dashboard/src/app/page.js
@@ -1,17 +1,40 @@
-import Image from "next/image";
+'use client';
+import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import GraphicArea from "../../components/GraphicArea/GraphicArea";
 import DiagramArea from "../../components/DiagrammArea/DiagramArea";
 import RequestLogArea from "../../components/RequestLogArea/RequestLogArea";
+
 export default function Home() {
+  const [selectedCaptureId, setSelectedCaptureId] = useState(null);
+
+  
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/captures')
+      .then(res => res.json())
+      .then(data => {
+        if (data && data.length > 0) {
+          setSelectedCaptureId(data[0].id); 
+        }
+      })
+      .catch(err => console.error("Error fetching captures:", err));
+  }, []);
+
   return (
     <div className={styles.page}>
       <h1>Network Traffic Overview</h1>
-      <div className={styles.GND}> 
-        <GraphicArea/>
-        <DiagramArea/>
-      </div>
-      <RequestLogArea/>
+      
+      {selectedCaptureId ? (
+        <>
+          <div className={styles.GND}> 
+            <GraphicArea captureId={selectedCaptureId} />
+            <DiagramArea captureId={selectedCaptureId} />
+          </div>
+          <RequestLogArea captureId={selectedCaptureId} />
+        </>
+      ) : (
+        <p>No captures found. Please run an analysis first.</p>
+      )}
     </div>
   );
 }
