4a03399fe59337d7db16262b47af9e8230b5f25a
Allakhverdiev Marat <115146711+Attract1ve@users.noreply.github.com>
2026-04-05 15:03:26 +0300

    main page was refactored

---
+++ frontend/dashboard/components/InfoPage/Infopage.js
@@ -0,0 +1,53 @@
+'use client';
+
+import { useState, useEffect } from "react";
+import styles from "./InfoPage.module.css";
+import DiagramArea from "../DiagrammArea/DiagramArea";
+import InfoArea from "../InfoArea/InfoArea";
+import RequestLogArea from "../RequestLogArea/RequestLogArea";
+
+export default function InfoPage() {
+  const [selectedCaptureId, setSelectedCaptureId] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/captures')
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && data.length > 0) {
+          setSelectedCaptureId(data[0].id);
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching captures:", err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.Title}>
+        <h1>Network Traffic Analytics Dashboard</h1>
+      </div>
+
+      {loading ? (
+        <p>Connecting to backend...</p>
+      ) : selectedCaptureId ? (
+        <>
+          <div className={styles.GND}>
+            <DiagramArea captureId={selectedCaptureId} />
+            <InfoArea captureId={selectedCaptureId} />
+          </div>
+
+          <RequestLogArea captureId={selectedCaptureId} />
+        </>
+      ) : (
+        <div className={styles.noData}>
+          <h2>No Data Available</h2>
+          <p>Please run the analysis script to populate the database.</p>
+        </div>
+      )}
+    </div>
+  );
+}
--- frontend/dashboard/src/app/page.js
+++ frontend/dashboard/src/app/page.js
@@ -5,49 +5,10 @@ import styles from "./page.module.css";
 import DiagramArea from "../../components/DiagrammArea/DiagramArea";
 import InfoArea from "../../components/InfoArea/InfoArea";
 import RequestLogArea from "../../components/RequestLogArea/RequestLogArea";
+import DragAndDrop from "../../components/DragAndDrop/DragAndDrop";
 
 export default function Home() {
-  const [selectedCaptureId, setSelectedCaptureId] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/captures')
-      .then((res) => res.json())
-      .then((data) => {
-        if (data && data.length > 0) {
-          setSelectedCaptureId(data[0].id);
-        }
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error("Error fetching captures:", err);
-        setLoading(false);
-      });
-  }, []);
-
   return (
-    <div className={styles.page}>
-      <div className={styles.Title}>
-        <h1>Network Traffic Analytics Dashboard</h1>
-      </div>
-
-      {loading ? (
-        <p>Connecting to backend...</p>
-      ) : selectedCaptureId ? (
-        <>
-          <div className={styles.GND}>
-            <DiagramArea captureId={selectedCaptureId} />
-            <InfoArea captureId={selectedCaptureId} />
-          </div>
-
-          <RequestLogArea captureId={selectedCaptureId} />
-        </>
-      ) : (
-        <div className={styles.noData}>
-          <h2>No Data Available</h2>
-          <p>Please run the analysis script to populate the database.</p>
-        </div>
-      )}
-    </div>
-  );
+    <DragAndDrop/>
+  )
 }
